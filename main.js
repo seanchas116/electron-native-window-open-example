804b006afceb7dc07e35ea01a0ba51b89711f0a4
Ryohei Ikegami <iofg2100@gmail.com>
2017-03-18 18:39:07 +0900

    Add basic files

---
+++ main.js
@@ -0,0 +1,7 @@
+const {app, BrowserWindow} = require("electron")
+
+app.on('ready', () => {
+  const win = new BrowserWindow({width: 800, height: 600})
+  win.loadURL("file://" + __dirname + "/index.html")
+  win.openDevTools()
+})
