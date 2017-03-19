3033c48256729d4e44831cca9599510702bfbee4
Ryohei Ikegami <iofg2100@gmail.com>
2017-03-19 15:13:36 +0900

    More real-world example

--- main.js
+++ main.js
@@ -1,16 +1,30 @@
-const {app, BrowserWindow} = require("electron")
+const {app, BrowserWindow, ipcMain} = require("electron")
 
 app.on('ready', () => {
   const win = new BrowserWindow({
     width: 800,
     height: 600,
   })
+  let modal = null
+  ipcMain.on("closeModal", () => {
+    if (modal) {
+      modal.close()
+      modal = null
+    }
+  })
   win.webContents.on('new-window', (event, url, frameName, disposition, options, additionalFeatures) => {
-    console.log(options)
-    event.preventDefault()
-    let win = new BrowserWindow(options)
-    win.loadURL(url)
-    event.newGuest = win
+    if (url.endsWith("modal.html")) {
+      event.preventDefault()
+      Object.assign(options, {
+        modal: true,
+        parent: win,
+        width: 100,
+        height: 100
+      })
+      modal = new BrowserWindow(options)
+      modal.loadURL(url)
+      event.newGuest = modal
+    }
   })
   win.loadURL("file://" + __dirname + "/index.html")
   win.openDevTools()
--- renderer.js
+++ renderer.js
@@ -1,6 +1,23 @@
-const win = window.open("./popup.html")
-win.onload = () => {
-  win.document.body.innerHTML = "from opener"
-  const browserWindow = win.require("electron").remote.getCurrentWindow()
-  browserWindow.maximize()
+const {ipcRenderer} = require("electron")
+
+const showModal = () => {
+  const modal = window.open("./modal.html")
+  modal.onload = () => {
+    const browserWindow = modal.require("electron").remote.getCurrentWindow()
+    const okButton = modal.document.getElementById("ok")
+    const cancelButton = modal.document.getElementById("cancel")
+    okButton.addEventListener("click", () => {
+      resultDiv.innerText = "OK"
+      // modal.close doesn't work now
+      ipcRenderer.send("closeModal")
+    })
+    cancelButton.addEventListener("click", () => {
+      resultDiv.innerText = "Cancel"
+      ipcRenderer.send("closeModal")
+    })
+  }
 }
+
+const showModalButton = document.getElementById("show-modal")
+const resultDiv = document.getElementById("result")
+showModalButton.addEventListener("click", showModal)
